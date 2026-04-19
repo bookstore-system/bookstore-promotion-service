e6806ca1d64cc3a15e8aac1305ea38bd6bf4806b
TruongIKPK <phitruong20192020@gmail.com>
2026-04-19 22:56:24 +0700

    first commit

---
+++ src/main/java/com/hamtech/bookstorepromotionservice/BookstorePromotionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorepromotionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorePromotionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstorePromotionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamtech/bookstorepromotionservice/BookstorePromotionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamtech.bookstorepromotionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstorePromotionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
