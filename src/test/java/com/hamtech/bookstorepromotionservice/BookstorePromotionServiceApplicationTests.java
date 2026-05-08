87655f512ecfce910645c552b738f587330e1e2b
HinDayNi <gthithanhhien04@gmail.com>
2026-05-08 14:39:22 +0700

    feat: enhance test configuration by adding mock PromotionRepository and excluding specific auto-configurations

--- src/test/java/com/hamtech/bookstorepromotionservice/BookstorePromotionServiceApplicationTests.java
+++ src/test/java/com/hamtech/bookstorepromotionservice/BookstorePromotionServiceApplicationTests.java
@@ -2,9 +2,19 @@ package com.hamtech.bookstorepromotionservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+import com.hamtech.bookstorepromotionservice.repository.PromotionRepository;
+
+@SpringBootTest(classes = BookstorePromotionServiceApplication.class, properties = {
+        "spring.autoconfigure.exclude=" +
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 class BookstorePromotionServiceApplicationTests {
+
+    @MockBean
+    PromotionRepository promotionRepository;
 
     @Test
     void contextLoads() {
