4af1eb0ecf9ba150acca529a5ecf0d8cfe561fe8
HinDayNi <gthithanhhien04@gmail.com>
2026-05-08 14:38:36 +0700

    feat: enable Feign clients for service communication in BookstorePromotionServiceApplication

--- src/main/java/com/hamtech/bookstorepromotionservice/BookstorePromotionServiceApplication.java
+++ src/main/java/com/hamtech/bookstorepromotionservice/BookstorePromotionServiceApplication.java
@@ -2,8 +2,10 @@ package com.hamtech.bookstorepromotionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BookstorePromotionServiceApplication {
 
     public static void main(String[] args) {
