02f42f4582f28a0c5f7c9ac4190b49408e769bb4
HinDayNi <gthithanhhien04@gmail.com>
2026-05-20 12:45:45 +0700

    feat: enable RabbitMQ support in the BookstorePromotionServiceApplication

--- src/main/java/com/hamtech/bookstorepromotionservice/BookstorePromotionServiceApplication.java
+++ src/main/java/com/hamtech/bookstorepromotionservice/BookstorePromotionServiceApplication.java
@@ -1,10 +1,12 @@
 package com.hamtech.bookstorepromotionservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableRabbit
 @EnableFeignClients
 public class BookstorePromotionServiceApplication {
 
